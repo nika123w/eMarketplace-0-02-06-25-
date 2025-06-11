3341449165d5fcccacb4acd18d7ad1fad22b4745
nika123w <nikusha.kurtiashvili@gmail.com>
2025-06-11 15:37:39 +0400

    Create V1__create_ads_table.sql

---
+++ V1__create_ads_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ads (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description TEXT NOT NULL,
+    submission_time TIMESTAMP NOT NULL,
+    photo_url TEXT
+);
