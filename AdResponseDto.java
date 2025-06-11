bc692a18101400953e1cc68e1aad2059682b2a9f
nika123w <nikusha.kurtiashvili@gmail.com>
2025-06-11 15:39:22 +0400

    Create AdResponseDto.java

---
+++ AdResponseDto.java
@@ -0,0 +1,8 @@
+public class AdResponseDto {
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    private LocalDateTime submissionTime;
+    private String photoUrl;
+}
